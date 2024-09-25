c22edec7d4fd4f7c2773ac65c876ece13df613f2
rohitnbt <rohitsinghnbt@gmail.com>
2024-09-25 21:56:59 +0530

    dark-mode-1

--- src/components/FlipTitle.tsx
+++ src/components/FlipTitle.tsx
@@ -6,7 +6,7 @@ export default function FlipTitle() {
 
   return (
       <h1 className="text-4xl md:text-6xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-950 to-neutral-400 dark:from-neutral-50 dark:to-neutral-400  bg-opacity-50">
-      Hi, I'm Rohit Singh: Creating Engaging<br />
+      Hi, I&apos;m Rohit Singh: Creating Engaging<br />
         <FlipWords words={words} /> <br />
         That Stand Out
       </h1>
