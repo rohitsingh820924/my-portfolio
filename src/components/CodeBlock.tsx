a55a8810a3e498ee6f1eea07a3dc22452a043402
rohitnbt <rohitsinghnbt@gmail.com>
2024-10-25 21:29:37 +0530

    blog-3 fix-1

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,9 +1,7 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { tomorrow } from 'react-syntax-highlighter/dist/cjs/styles/prism'; // Importing styles for both dark and light modes
-import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { tomorrow } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { useState } from 'react';
-import { FiClipboard, FiCheck } from 'react-icons/fi'; // Icons for copy and copied status
-import { useTheme } from 'next-themes';
+import { FiClipboard, FiCheck } from 'react-icons/fi';
 
 interface CodeBlockProps {
   code: string;
@@ -12,7 +10,6 @@ interface CodeBlockProps {
 
 const CodeBlock: React.FC<CodeBlockProps> = ({ code, language }) => {
   const [copied, setCopied] = useState(false);
-  const { theme } = useTheme();
 
   const handleCopy = () => {
     navigator.clipboard.writeText(code);
@@ -27,14 +24,12 @@ const CodeBlock: React.FC<CodeBlockProps> = ({ code, language }) => {
         aria-label="Copy code"
         className='absolute top-3 right-3'
       >
-        {copied ? <FiCheck color={theme==="dark" ? "white" : "black"} /> : <FiClipboard color={theme==="dark" ? "white" : "black"} />}
+        {copied ? <FiCheck color={"white"} /> : <FiClipboard color={"white"} />}
       </button>
 
       <SyntaxHighlighter 
         language={language} 
-        style={theme==="dark" ? tomorrow : solarizedlight} // Conditional styling based on isDarkMode
-        showLineNumbers
-        customStyle={theme === "dark" ? {} : { backgroundColor: "#e5e7eb" }}
+        style={ tomorrow } 
       >
         {code}
       </SyntaxHighlighter>
