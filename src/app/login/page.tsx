0132afe885a7fa90c8ec4819e944710364579113
rohitnbt <rohitsinghnbt@gmail.com>
2024-11-25 21:35:43 +0530

    email fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,15 +4,17 @@ import { apiGet } from '@/lib/api'
 
 const baseUrl = process.env.NEXT_BASE_URL;
 
-async function getOtp() {
-  await apiGet(`${baseUrl}/api/send-otp`)
+async function getOtp(): Promise<void> {
+  await apiGet(`${baseUrl}/api/send-otp`);
 }
 
-export default async function page() {
+const Page: React.FC = async () => {
   await getOtp();
   return (
-    <div className='flex min-h-svh items-center justify-center dark:bg-black bg-white antialiased '>
+    <div className='flex min-h-svh items-center justify-center dark:bg-black bg-white antialiased'>
         <Login />
     </div>
   )
 }
+
+export default Page;
