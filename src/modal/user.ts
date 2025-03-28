5f297509c972ef5bd80e7e6ed05328025cce0791
rohitnbt <rohitsinghnbt@gmail.com>
2025-03-28 22:11:39 +0530

    tiffine-6

--- src/modal/user.ts
+++ src/modal/user.ts
@@ -10,7 +10,7 @@ const UserSchema = new Schema<IUser>(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    googleId: { type: String, unique: true },
+    googleId: { type: String },
   },
   { timestamps: true }
 );
